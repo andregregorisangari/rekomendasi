4d79cc5241e9650766d55704f8f4052b604d1c6e
Andre Gregori Sangari <andresangari12@gmail.com>
2024-06-19 19:02:15 +0700

    first commit

--- main.py
+++ main.py
@@ -153,4 +153,4 @@ def recommend():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
